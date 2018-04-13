a6c3626e7cb6e558f6d7475de9a1f0906b48ebc5
Becky828 <38361070+Becky828@users.noreply.github.com>
2018-04-13 12:32:48 -0700

    Add files via upload

--- MinCostThruHexTiledMap/src/shortestPath/DijkstraAlg.java
+++ MinCostThruHexTiledMap/src/shortestPath/DijkstraAlg.java
@@ -3,17 +3,36 @@
  */
 package shortestPath;
 
+import java.util.Arrays;
+
 /**
  * @author Owner
  *
  */
 class DijkstraAlg {
+	int currentVertex;
+	int MAXV;
+	int start = 226;
+	int MAXINT = 9999;
+	int w;
+	int currentOptimalWeight;
+	int dist;
+	Boolean[] inTree = new Boolean[MAXV + 1];
+	Integer[] distance = new Integer[MAXV + 1];
+	Integer[] parent = new Integer[MAXV + 1];
 
 	/**
-	 * 
-	 */
+		 * 
+		 */
 	public DijkstraAlg() {
-		// TODO Auto-generated constructor stub
+		Arrays.fill(inTree, Boolean.FALSE);
+		Arrays.fill(distance, MAXINT);
+		Arrays.fill(parent, -1);
+		distance[start]= 0;
+	currentVertex = start;
+	while(!inTree[currentVertex]){
+	inTree[currentVertex] = Boolean.TRUE;
 	}
 
+}
 }
